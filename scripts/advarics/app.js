9fc45ea638709fb3e69f95b9ddc7e260a5304b0b
brakmic <brakmic@gmail.com>
2015-03-05 21:12:13 +0100

    cleanup

--- scripts/advarics/app.js
+++ scripts/advarics/app.js
@@ -3,9 +3,9 @@ var Chart = require('adv-chart');
 
 function domLoaded (e) {
    var advChart = new Chart();
-  advChart.registerComponent();
-  advChart.activateComponent();
-  console.log('app started');
+   advChart.registerComponent();
+   advChart.activateComponent();
+   console.log('app started');
 }
 document.addEventListener("load", domLoaded, false);
 document.addEventListener("DOMContentLoaded", domLoaded, false);
