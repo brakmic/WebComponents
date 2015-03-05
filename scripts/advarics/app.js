8be40f8a0917b285b8c00cf516436ffd54ef46be
brakmic <brakmic@gmail.com>
2015-03-05 21:10:52 +0100

    DOMContentLoaded

--- scripts/advarics/app.js
+++ scripts/advarics/app.js
@@ -1,9 +1,11 @@
 'use strict';
 var Chart = require('adv-chart');
-//main entry point
-$(function(){
-  var advChart = new Chart();
+
+function domLoaded (e) {
+   var advChart = new Chart();
   advChart.registerComponent();
   advChart.activateComponent();
   console.log('app started');
-});
+}
+document.addEventListener("load", domLoaded, false);
+document.addEventListener("DOMContentLoaded", domLoaded, false);
